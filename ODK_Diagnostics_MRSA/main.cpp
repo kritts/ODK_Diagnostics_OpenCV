7808f74ae55c184249484a4908b449027c0a5559
unknown <krittika.dsilva@gmail.com>
2014-05-03 17:08:43 +0100

    Ameliorated algorithm - constrast is increased before thresholding

--- ODK_Diagnostics_MRSA/main.cpp
+++ ODK_Diagnostics_MRSA/main.cpp
@@ -44,70 +44,99 @@ int main(int argc, char** argv)
 	int rows = blue_channel.rows;
 	int cols = blue_channel.cols;
 
-	// Roughly cropping the image 
-	Mat cropedImage = blue_channel(Rect(cols / 8, rows * 3 / 8, cols * 7 / 8 - cols / 8, rows * 3 / 4 - rows * 3 / 8));
-	Mat original_cropped = src(Rect(cols / 8, rows * 3 / 8, cols * 7 / 8 - cols / 8, rows * 3 / 4 - rows * 3 / 8));
 
+	int originalX = cols / 8;
+	int originalY = rows * 3 / 8;
+	int width = cols * 7 / 8 - originalX;
+	int height = rows * 3 / 4 - rows * 2 / 8;
+
+
+	// Roughly cropping the image 
+	Mat cropedImage = blue_channel(Rect(originalX, originalY, width, height));
+	Mat original_cropped = src(Rect(originalX, originalY, width, height));
+	
 	// Blur the image  (necessary step?)
 	Mat croppedBlurred;
-	GaussianBlur(cropedImage, croppedBlurred, Size(5, 5), 10.0);
+	GaussianBlur(cropedImage, croppedBlurred, Size(1, 1), 10.0);
 
+	// Increase constrast 
+	equalizeHist(croppedBlurred, croppedBlurred);
+	 
+	// Additional threshold (necessary step?)
+	//croppedBlurred = croppedBlurred > 150;
+
+	
 	// Make the image "black and white" by examining pixels over a certain intensity only (high threshold) 
-	croppedBlurred = croppedBlurred > 150;
+	threshold(croppedBlurred, croppedBlurred, // input and output 
+			  100,							  // treshold value 
+			  255,							  // max binary value 
+			  THRESH_BINARY | THRESH_OTSU);   // required flag to perform Otsu thresholding
+	
 	 
 	Mat output;
-	int erosion_size = 3;
+	int erosion_size = 3;	// for fiducials 
 	Mat element = getStructuringElement(MORPH_CROSS, Size(2 * erosion_size + 1, 2 * erosion_size + 1), Point(erosion_size, erosion_size));
 	Mat dilated;
-
-
+	 
 	// Apply the erosion operation
-		erode(croppedBlurred, croppedBlurred, element);
-	 	dilate(croppedBlurred, croppedBlurred, element);
+	erode(croppedBlurred, croppedBlurred, element);
+	dilate(croppedBlurred, croppedBlurred, element);
 
+
+	erosion_size = 7;		// for region outside test 
+	element = getStructuringElement(MORPH_CROSS, Size(2 * erosion_size + 1, 2 * erosion_size + 1), Point(erosion_size, erosion_size));
+	erode(croppedBlurred, croppedBlurred, element);
+	dilate(croppedBlurred, croppedBlurred, element);
+	  
 	vector<Vec3f> circles;
 	// Apply the Hough Transform to find the circles
 	HoughCircles(croppedBlurred, circles, CV_HOUGH_GRADIENT,
-		1,								// accumulator resolution (size of the image / 2)
-		croppedBlurred.cols / 4,		// minimum distance between two circles
-		1500,							// Canny high threshold
-		10,								// minimum number of votes
-		10, 20);						// min and max radius
+				1,								// accumulator resolution (size of the image / 2)
+				croppedBlurred.cols / 8,		// minimum distance between two circles
+				1500,							// Canny high threshold
+				10,								// minimum number of votes
+				10, 25);						// min and max radius
 	std::cout << "Number of circles found: ";
 	std::cout << circles.size();
 
+
+	int rowsCropped = original_cropped.rows;
+	int colsCropped = original_cropped.cols;
+
 	// Draw the circles detected
-	for (size_t i = 0; i < circles.size(); i++) 
+	for (size_t i = 0; i < circles.size(); i++)
 	{
 		Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
 		int radius = cvRound(circles[i][2]);
-		// circle center
-		circle(original_cropped, center, 3, Scalar(94, 206, 165), -1, 8, 0);
-		// circle outline
-		circle(original_cropped, center, radius, Scalar(94, 206, 165), 3, 8, 0);
+		if (center.x <= colsCropped / 2 && center.y <= rowsCropped / 2)
+		{
+			// circle center
+			circle(original_cropped, center, 3, Scalar(94, 206, 165), -1, 8, 0);
+			// circle outline
+			circle(original_cropped, center, radius, Scalar(94, 206, 165), 3, 8, 0);
 
-		std::cout << "\n";
-		std::cout << i;
-		std::cout << ": x-position: ";
-		std::cout << cvRound(circles[i][0]);
-		std::cout << "\t";
-		std::cout << "y-position: ";
-		std::cout << cvRound(circles[i][1]);
+			std::cout << "\n";
+			std::cout << i;
+			std::cout << ": x-position: ";
+			std::cout << cvRound(circles[i][0]);
+			std::cout << "\t";
+			std::cout << "y-position: ";
+			std::cout << cvRound(circles[i][1]);
+		}
 	}
-
-	//	Mat original_cropped2 = src(Rect(cols / 8, rows * 3 / 8, cols * 7 / 8 - cols / 8, rows * 3 / 4 - rows * 3 / 8));
-
+	std::cout << "\n";
+	 
 
 
 	//	namedWindow("Original", CV_WINDOW_FREERATIO);
-	// 	namedWindow("Blue Channel", CV_WINDOW_FREERATIO);
-	//	namedWindow("Blue Channel + Cropped", CV_WINDOW_FREERATIO); 
-	namedWindow("Original Photo - With Fiducials", CV_WINDOW_FREERATIO);
+	//	namedWindow("Blue Channel", CV_WINDOW_FREERATIO);
+		namedWindow("Blue Channel + Cropped", CV_WINDOW_FREERATIO); 
+		namedWindow("Original Photo - With Fiducials", CV_WINDOW_FREERATIO);
 
 	//	imshow("Original", src);
 	//	imshow("Blue Channel", blue_channel);
-	// 	imshow("Blue Channel + Cropped", blue_channel);
-	imshow("Original Photo - With Fiducials", original_cropped);
+		imshow("Blue Channel + Cropped", croppedBlurred);
+		imshow("Original Photo - With Fiducials", original_cropped);
 
 	waitKey(0);
 
